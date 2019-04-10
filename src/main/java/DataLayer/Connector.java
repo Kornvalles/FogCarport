edf0f465bd714bc11a9aa5323945c61b6a37f31b
Mikkel Kornval Christoffersen <mikkel@Mikkels-MacBook-Pro.local>
2019-04-10 15:15:08 +0200

    Create Connector.java

---
+++ src/main/java/DataLayer/Connector.java
@@ -0,0 +1,49 @@
+package DataLayer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * The purpose of Connector is to...
+ *
+ * @author kasper
+ */
+public class Connector {
+
+    //Constants
+    private static final String IP = "localhost";
+    private static final String PORT = "3306";
+    public static final String DATABASE = "LegoHouse";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private static Connection singleton;
+
+    public static void setConnection(Connection con) {
+        singleton = con;
+    }
+
+    public static synchronized Connection connection() throws SQLException, ClassNotFoundException {
+        if (singleton == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                String url = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE;
+                Properties props = new Properties();
+                props.put("user", USERNAME);
+                props.put("password", PASSWORD);
+                props.put("allowMultiQueries", true);
+                props.put("useUnicode", true);
+                props.put("useJDBCCompliantTimezoneShift", true);
+                props.put("useLegacyDatetimeCode", false);
+                props.put("serverTimezone", "CET");
+                Connector.singleton = DriverManager.getConnection(url, props);
+            } catch (ClassNotFoundException ex) {
+                throw new SQLException(ex.getMessage());
+            }
+        }
+        return singleton;
+    }
+
+}
