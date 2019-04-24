5da22dcd972b926610f18415f0f4ea136c8b1239
benjaminbajrami <benjaminbajrami@10.50.131.71>
2019-04-24 15:01:03 +0200

    Modified Carport entity to hold details of standard carports and created method getStandard that gives a list of carports for the customer.

--- src/main/java/DataLayer/OrderMapper.java
+++ src/main/java/DataLayer/OrderMapper.java
@@ -1,9 +1,50 @@
 package DataLayer;
 
+import FunctionLayer.Carport;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @authors Iben, Christian, Benjamin, Nicklas, Mikkel
  */
 public class OrderMapper {
+//establish database connection
+    
+    private Connector conn;
+    
+/** Returns a list of all toppings
+     * @return  */
+    public List<Carport> getStandard() throws ClassNotFoundException {
+        try {
+            conn = new Connector();
 
+            String query = "SELECT * FROM `Carport`.`standard`;";
+
+            Connection connection = conn.connection();
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            List<Carport> standards = new ArrayList<>();
+            int id = 0;
+            String details = "";
+            int price = 0;
+
+            while (rs.next()) {
+                details = rs.getString("details");
+                price = rs.getInt("price");
+                id = rs.getInt("id");
+                Carport standard = new Carport(id, price, false, details);
+                standards.add(standard);
+            }
+            return standards;
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return null;
+    }
 }
--- src/main/java/FunctionLayer/Carport.java
+++ src/main/java/FunctionLayer/Carport.java
@@ -14,11 +14,13 @@ public class Carport {
     private final int height; //measured in centimeters.
     private final int length; //measured in centimeters.
     private final boolean toolshed; //false when toolshed is not opted
+    private final String details; //details on carport
 
-    public Carport(int height, int length, boolean toolshed) {
+    public Carport(int height, int length, boolean toolshed, String details) {
         this.height = height;
         this.length = length;
         this.toolshed = toolshed;
+        this.details = details;
     }
 
     public int getHeight() {
