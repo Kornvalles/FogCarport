7363f8c088402fd2a2883af576f60cf8a3f06710
Iben A <cph-ia62@cphbusiness.dk>
2019-04-28 13:36:03 +0200

    Test startup

---
+++ src/test/java/Test_Carport/MakeCarport.java
@@ -0,0 +1,21 @@
+package Test_Carport;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ibenk
+ */
+public class MakeCarport {
+    
+    public MakeCarport() {
+    }
+
+    @Test
+    public void makeCarport() {
+    
+    }
+    
+    
+}
