445eac2aed715d2563f87f5198573b12a26186f7
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-04-29 23:19:54 +0200

    Update SQLscript.sql

--- src/main/java/DataLayer/SQLscript.sql
+++ src/main/java/DataLayer/SQLscript.sql
@@ -1,8 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  * Author:  ndupo
  * Created: Apr 24, 2019
@@ -13,39 +8,27 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Schema fog
--- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema fog
+-- Schema FogCarport
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `fog` DEFAULT CHARACTER SET utf8 ;
-USE `fog` ;
-
--- -----------------------------------------------------
--- Table `fog`.`employee`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `fog`.`employee` (
-  `employeeID` VARCHAR(16) NOT NULL,
-  `name` VARCHAR(255) NULL,
-  `password` VARCHAR(45) NULL,
-  PRIMARY KEY (`employeeID`));
+CREATE SCHEMA IF NOT EXISTS `FogCarport` DEFAULT CHARACTER SET utf8 ;
+USE `FogCarport` ;
 
 
 -- -----------------------------------------------------
--- Table `fog`.`customer`
+-- Table `FogCarport`.`customer`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `fog`.`customer` (
+CREATE TABLE IF NOT EXISTS `FogCarport`.`customer` (
   `name` VARCHAR(16) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`email`));
 
 
 -- -----------------------------------------------------
--- Table `fog`.`material`
+-- Table `FogCarport`.`material`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `fog`.`material` (
+CREATE TABLE IF NOT EXISTS `FogCarport`.`material` (
   `materialID` INT NOT NULL,
   `name` VARCHAR(255) NOT NULL,
   `MSRP` DOUBLE NOT NULL,
@@ -55,67 +38,35 @@ CREATE TABLE IF NOT EXISTS `fog`.`material` (
 
 
 -- -----------------------------------------------------
--- Table `fog`.`carport`
+-- Table `FogCarport`.`order`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `fog`.`carport` (
-  `carportID` INT NOT NULL AUTO_INCREMENT,
-  `carportWS` TINYINT NOT NULL,
-  `materialID` INT NOT NULL,
-  `orderID` INT NOT NULL,
-  `quantity` INT NOT NULL,
-  `length` INT NOT NULL,
-  `width` INT NOT NULL,
-  `height` INT NOT NULL,
-  `comment` VARCHAR(45) NULL,
-  PRIMARY KEY (`carportID`),
-  INDEX `material_ID_idx` (`materialID` ASC) VISIBLE,
-  INDEX `order_ID_idx` (`orderID` ASC) VISIBLE,
-  CONSTRAINT `material_ID`
-    FOREIGN KEY (`materialID`)
-    REFERENCES `fog`.`material` (`materialID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `order_ID`
-    FOREIGN KEY (`orderID`)
-    REFERENCES `fog`.`order` (`orderID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-
--- -----------------------------------------------------
--- Table `fog`.`order`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `fog`.`order` (
-  `orderID` INT NOT NULL AUTO_INCREMENT,
-  `carportID` INT NOT NULL,
-  `employeeID` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `FogCarport`.`order` (
+  `orderId` INT NOT NULL AUTO_INCREMENT,
+  `employeeId` INT NOT NULL,
+  `customerId` INT NOT NULL,
   `customerEmail` VARCHAR(255) NOT NULL,
+  `customerName` VARCHAR(255) NOT NULL,
   `carportHeight` INT NOT NULL,
   `carportWidth` INT NOT NULL,
-  `carportLength` INT NOT NULL,
+  `carportLength` INT NOT NULL DEFAULT 230,
   `rooftopType` VARCHAR(45) NOT NULL,
-  `shedLength` INT NULL,
-  `shedWidth` INT NULL,
+  `shed` BOOLEAN,
   `totalPrice` DOUBLE NOT NULL,
   PRIMARY KEY (`orderID`),
-  INDEX `employeeID_idx` (`employeeID` ASC) VISIBLE,
-  INDEX `customerEmail_idx` (`customerEmail` ASC) VISIBLE,
-  INDEX `carportID_idx` (`carportID` ASC) VISIBLE,
-  CONSTRAINT `employeeID`
-    FOREIGN KEY (`employeeID`)
-    REFERENCES `fog`.`employee` (`employeeID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `customerEmail`
-    FOREIGN KEY (`customerEmail`)
-    REFERENCES `fog`.`customer` (`email`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `carportID`
-    FOREIGN KEY (`carportID`)
-    REFERENCES `fog`.`carport` (`carportID`)
+    FOREIGN KEY (`employeeId`)
+    REFERENCES `employee` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table `fog`.`employee`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `FogCarport`.`employee` (
+  `id` VARCHAR(16) NOT NULL,
+  `name` VARCHAR(255) NULL,
+  `password` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
