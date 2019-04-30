42240cea9f6a7dd2bb49accb346e0987fb93e132
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-04-30 10:13:39 +0200

    Update LogicFacade.java

--- src/main/java/FunctionLayer/LogicFacade.java
+++ src/main/java/FunctionLayer/LogicFacade.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
  */
 public class LogicFacade {
 
-    public static void addCustomer( Customer customer ) throws FogException {
+    public static void addCustomer( Customer customer ) throws FogException, SQLException {
         UserMapper.addCustomer(customer);
     }
 
