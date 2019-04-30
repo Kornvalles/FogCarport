1bbaf4facbc26fd413229045f5c383dec3acfa46
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-04-30 16:09:36 +0200

    Create User.java

---
+++ src/main/java/PresentationLayer/User.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PresentationLayer;
+
+import FunctionLayer.FogException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author mikkel
+ */
+public class User extends Command {
+
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws FogException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
