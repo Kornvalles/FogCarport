ebec43b6cf1ca5cbdbe13fed523a5f9a957240f6
Iben A <cph-ia62@cphbusiness.dk>
2019-05-02 16:29:51 +0200

    Update CreateOrderCommand.java

--- src/main/java/PresentationLayer/CreateOrderCommand.java
+++ src/main/java/PresentationLayer/CreateOrderCommand.java
@@ -21,9 +21,9 @@ public class CreateOrderCommand extends Command {
 
     @Override
     String execute(HttpServletRequest request, LogicFacade logic) throws FogException {
-        
+
         HttpSession session = request.getSession();
-        
+
         //Fanger inputs fra jsp som ligger på requestet.
         String reqLength = request.getParameter("length");
         String reqWidth = request.getParameter("width");
@@ -35,30 +35,29 @@ public class CreateOrderCommand extends Command {
         String reqAddress = request.getParameter("address");
         String reqZipcode = request.getParameter("zipcode");
         String reqPhonenumber = request.getParameter("phonenumber");
-        
+
         //Konveterer inputs som ikke skal være String.
         boolean shed = Boolean.parseBoolean(reqShed);
         boolean roof = Boolean.parseBoolean(reqRoof);
         boolean wall = Boolean.parseBoolean(reqWall);
         int length = Integer.parseInt(reqLength);
         int width = Integer.parseInt(reqWidth);
-        
+
         //Instancerer objekter og putter dem på session
-        Customer customer = new Customer(reqName, reqEmail, reqAddress, reqZipcode, reqPhonenumber);
-        Carport carport = new Carport(230, length, width, shed, roof, wall, "");
-        Construction construction = Calculator.constructCarport(carport, logic);
-        session.setAttribute("construction", construction);
-        session.setAttribute("customer", customer);
-        
         //Kører logikken. Forsøger at putte ting i database.
         try {
+            Customer customer = new Customer(reqName, reqEmail, reqAddress, reqZipcode, reqPhonenumber);
+            Carport carport = new Carport(230, length, width, shed, roof, wall, "");
+            Construction construction = Calculator.constructCarport(carport, logic);
+            session.setAttribute("construction", construction);
+            session.setAttribute("customer", customer);
             logic.addCustomer(customer);
             logic.createOrder(carport, customer);
         } catch (SQLException ex) {
             System.out.println(ex.getLocalizedMessage());
             System.out.println(ex.getMessage());
         }
-        
+
         //Sender en String tilbage til Frontcontrolleren om hvilken jsp side vi skal omdirigeres til.
         return "confirmation";
     }
