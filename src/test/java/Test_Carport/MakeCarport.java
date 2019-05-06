389ff92022365dbe43f84c9e6a3abcb4edbe1c0c
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-05-06 10:46:48 +0200

    Update MakeCarport.java

--- src/test/java/Test_Carport/MakeCarport.java
+++ src/test/java/Test_Carport/MakeCarport.java
@@ -3,18 +3,11 @@ package Test_Carport;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author ibenk
- */
 public class MakeCarport {
     
-    public MakeCarport() {
-    }
-
     @Test
     public void makeCarport() {
-    
+        
     }
     
     
