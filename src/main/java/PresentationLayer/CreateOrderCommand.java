9ddc29353336dea9aa872f59119de6100046c170
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-05-15 09:32:02 +0200

    Invoice update

--- src/main/java/PresentationLayer/CreateOrderCommand.java
+++ src/main/java/PresentationLayer/CreateOrderCommand.java
@@ -6,12 +6,8 @@ import FunctionLayer.Construction;
 import FunctionLayer.Customer;
 import FunctionLayer.FogException;
 import FunctionLayer.LogicFacade;
-import FunctionLayer.Shed;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
