0e2d606ef308eb4ffb17d7422c5eeb010d53cb72
Benjamin Bajrami <Aeydin24@gmail.com>
2019-05-16 09:46:13 +0200

    Update DatabaseLogicFacade.java

--- src/main/java/FunctionLayer/DatabaseLogicFacade.java
+++ src/main/java/FunctionLayer/DatabaseLogicFacade.java
@@ -69,11 +69,6 @@ public class DatabaseLogicFacade implements LogicFacade {
      * @throws SQLException
      */
     @Override
-    public void getEmployee(Employee employee) throws FogException, SQLException {
-        UserMapper.getEmployee(employee);
-    }
-
-    @Override
     public int getMaterialId(String name) throws FogException, SQLException {
         return OrderMapper.getMaterialId(name);
     }
