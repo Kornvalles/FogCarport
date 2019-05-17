220558851305f4595acf4be9643b9edfc9696798
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-05-17 19:33:49 +0200

    Merge

--- src/test/java/TestDataLayer/OrderMapperTest.java
+++ src/test/java/TestDataLayer/OrderMapperTest.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package DataLayer;
+package TestDataLayer;
 
+import DataLayer.OrderMapper;
 import FunctionLayer.Carport;
 import FunctionLayer.Customer;
 import org.junit.After;
