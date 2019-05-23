e2bf8f339ac52459d8cd25164b59fd1426a730a8
Aeydin24 <44898406+Aeydin24@users.noreply.github.com>
2019-05-24 00:10:25 +0200

    Create DeleteOrderCommand.java

---
+++ src/main/java/PresentationLayer/DeleteOrderCommand.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PresentationLayer;
+
+import FunctionLayer.FogException;
+import FunctionLayer.LogicFacade;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Benjamin
+ */
+//skal fikses
+public class DeleteOrderCommand extends Command {
+
+    @Override
+    String execute(HttpServletRequest request, LogicFacade logic) throws FogException {
+        return null;
+    }
+    
+    
+    
+}
