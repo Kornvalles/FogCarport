25513dc519ed56e12a63416c235fd87fd4d06702
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-05-29 11:23:03 +0200

    Update DeleteOrderCommand.java

--- src/main/java/PresentationLayer/DeleteOrderCommand.java
+++ src/main/java/PresentationLayer/DeleteOrderCommand.java
@@ -1,24 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PresentationLayer;
 
 import FunctionLayer.FogException;
 import FunctionLayer.LogicFacade;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Benjamin
- */
 public class DeleteOrderCommand extends Command {
+    
+    private static final Logger logger = Logger.getLogger(ChangePriceCommand.class.getName());
 
     @Override
     String execute(HttpServletRequest request, LogicFacade logic) throws FogException {
-
+        HttpSession session = request.getSession();
         logic.deleteOrder(Integer.parseInt(request.getParameter("orderid")));
+        try {
+            session.setAttribute("orderlist", logic.getAllOrders());
+        } catch (SQLException ex) {
+            logger.getLogger(DeleteOrderCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return "viewAllOrders";
     }
 }
